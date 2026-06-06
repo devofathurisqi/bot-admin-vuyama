d06ec56fad876124a0e4f2f02554d91555b89076
devofathurisqi <devofathurisqi3@gmail.com>
2026-06-06 17:16:46 +0700

    fix boot crash by removing deleted recover_images module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const { startBot } = require('./bot');
 const { startServer } = require('./server');
-const { recoverImages } = require('./scripts/recover_images');
 const logger = require('./utils/logger');
 
 // Start application components
@@ -10,9 +9,6 @@ const main = async () => {
     
     // 1. Boot up Express Backend & Socket.IO
     startServer();
-    
-    // 2. Run Image Recovery module in background
-    recoverImages().catch(err => logger.error('Error in startup image recovery:', err));
     
     // 3. Boot up WhatsApp Bot Client
     await startBot();
